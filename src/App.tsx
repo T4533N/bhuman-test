7f12b8357f386350214b812f995e0bb9b2526a93
T4533N <taseentanvir@gmail.com>
2022-10-13 15:16:25 +0600

    change sidebar width

--- src/App.tsx
+++ src/App.tsx
@@ -410,7 +410,7 @@ const App: Component = () => {
         <div class="bottom-0 w-full h-12 sticky bg-gray-100 grid place-items-center border-t border-gray-300 border-r">
           <div class="flex items-center justify-between px-2 py-3 w-full">
             <div class="flex gap-[2px]">
-              <select class="bg-white border border-gray-300 rounded-[4px] text-[13px] font-normal leading-6 text-gray-500 appearance-none select-custom px-5">
+              <select class="bg-white border border-gray-300 rounded-[4px] text-[13px] font-normal leading-6 text-gray-600 appearance-none select-custom px-5">
                 <option>25</option>
                 <option>50</option>
                 <option>100</option>
@@ -442,7 +442,7 @@ const App: Component = () => {
               <div class="flex items-center gap-1">
                 <div class="bg-white border border-gray-300 grid place-items-center p-1 rounded-[4px]">
                   <input
-                    class="text-[13px] text-black leading-4 font-normal w-4 h-4 outline-none text-center"
+                    class="text-[13px] text-black placeholder-gray-600 leading-4 font-normal w-4 h-4 outline-none text-center"
                     placeholder="1"
                     type="text"
                   />
@@ -546,7 +546,7 @@ const App: Component = () => {
       {/* sidepanel */}
       <div
         class={`${
-          sidePanel() ? "w-[130%]" : "w-0"
+          sidePanel() ? "w-[250%]" : "w-0"
         } bg-white z-50 transition-all duration-200 ease-in-out h-screen border-l rounded-tl-xl rounded-bl-xl border-gray-300`}
       />
 
